0bfbd7c16f984c2d686c08281a2d6eade62c585f
Patric Gibson Lima <TIJ0158790@aluno.firjansenaisesi.com.br>
2025-08-12 20:08:04 -0300

    Adicionando Classe Produto e Endereco

---
+++ demo/src/main/java/com/Erp/demo/model/Produto.java
@@ -0,0 +1,41 @@
+package com.Erp.demo.model;
+
+public class Produto {
+	private int idProduto;
+	private String descricao;
+	private double preco;
+	private int estoqueAtual;
+
+public Produto( int idProduto, String descricao, double preco, int estoqueAtual) {
+	this.idProduto = idProduto;
+	this.descricao = descricao;
+	this.preco = preco;
+	this.estoqueAtual = estoqueAtual;
+}
+	
+	public int getIdProduto() {
+		return idProduto;
+	}
+	public void setIdProduto(int idProduto) {
+		this.idProduto = idProduto;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public int getEstoqueAtual() {
+		return estoqueAtual;
+	}
+	public void setEstoqueAtual(int estoqueAtual) {
+		this.estoqueAtual = estoqueAtual;
+	}	
+
+}
